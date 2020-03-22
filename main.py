cbc056d65913dd9a32dd67390d0caf62dad06a5e
Vatraz <grubiszko@gmail.com>
2020-03-22 23:47:40 +0100

    fixed number detection

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import cv2
 import numpy as np
 
-from circle_detction import detect_circles, draw_circles
+from circle_detction import detect_circles, draw_circles, erase_circles
+from number_detection import detect_numbers
 
-image = cv2.imread('img\image2.jpg', cv2.IMREAD_COLOR)
 
+
+image = cv2.imread('img\(1).jpg', cv2.IMREAD_COLOR)
+h, w = image.shape[:2]
+
+if w > h:
+    image = cv2.resize(image, (1200, int(h * float(1200 / w))), cv2.INTER_LINEAR)
+else:
+    image = cv2.resize(image, (int(w * float(1200 / h)), 1200))
+
+# M = cv2.getRotationMatrix2D((10, 100), 45, 1)
+# print(M)
 circles = detect_circles(image)
-draw_circles(image, circles)
+# image = erase_circles(image, circles)
+# draw_circles(image, circles)
+detect_numbers(image, circles)
--- number_detection.py
+++ number_detection.py
@@ -5,79 +5,130 @@ import cv2
 
 
 shape_x, shape_y = (128, 128)
-digit_w_min, digit_w_max = (6, 50)
-digit_h_min, digit_h_max = (17, 35)
+digit_w_min, digit_w_max = (6, 55)
+digit_h_min, digit_h_max = (17, 65)
+circle_max = 20
 
-def erase_circle(image):
-    # shape = image.shape[0]
-    # color = 255
-    # cv2.rectangle(image, (shape, y-r), (x+r, y+r), color=color, thickness=cv2.FILLED)
-    return image
+
 
 
 def contour_is_circle(bounding_rect):
     (x, y, w, h) = bounding_rect
-    if abs(h - w) < 2 and w <= digit_w_min and h < digit_h_min:
+    if abs(h - w) <= 3 and w <= circle_max and h <= circle_max:
         return True
     return False
 
 
-def detect_numbers(image, circles):
-    image = image.copy()
-    width, height, _ = image.shape
+def get_circle_image(image, circle):
+    height, width = image.shape[:2]
+    x, y, _ = circle
     offset = int(width / 30)
+    offset_x_left = offset_x_right = offset_y_up = offset_y_bottom = offset
+    temp = np.full((offset * 2, offset * 2), fill_value=255, dtype=np.uint8)
+
+    if width - offset > x > offset and offset < y < height - offset:
+        cropped = image[y - offset:y + offset, x - offset:x + offset]
+        return cropped
+
+    if x < offset:
+        offset_x_left = x
+    if x > width - offset:
+        offset_x_right = width - x
+    if y < offset:
+        offset_y_up = y
+    if y > height - offset:
+        offset_y_bottom = height - y
+    cropped = image[y - offset_y_up:y + offset_y_up, x - offset_x_left:x + offset_x_right]
+    temp[offset-offset_y_up:offset+offset_y_bottom, offset-offset_x_left:offset+offset_x_right] = cropped
+    return temp
+
+
+def circle_bounding_rect(contours, erase_img_list=(), erase_color=0):
+    found_circle = True
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if int(shape_x * 0.4) < int(x + w / 2) < int(shape_x * 0.6) and int(shape_y * 0.4) < int(y + h / 2) < int(
+                shape_y * 0.6):
+            print('JESTEM W SRODECZKU :>')
+            print((x, y, w, h))
+            if not contour_is_circle([x, y, w, h]):
+                print('I TO NIE JEST  KUŁECZKO :<')
+                found_circle = False
+            for image in erase_img_list:
+                cv2.rectangle(image, (x, y), (x + w, y + h), color=erase_color, thickness=cv2.FILLED)
+    return found_circle
+
+
+def detect_numbers(image, circles):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     for circle in circles:
-        break_loop = False
         x, y, _ = circle
-        try:
-            temp = cv2.resize(image[y - offset:y + offset, x - offset:x + offset], (shape_x, shape_y))
-            gray = cv2.cvtColor(temp, cv2.COLOR_BGR2GRAY)
-            gray = erase_circle(gray)
-            gray_blur = cv2.blur(gray, (3, 3))
 
-            _, gray_thresh = cv2.threshold(gray_blur, 160, 255, cv2.THRESH_BINARY_INV)
+        cropped = get_circle_image(image, circle)
+        circle_image = cv2.resize(cropped, (shape_x, shape_y))
+        # gray_blur = cv2.blur(gray, (3, 3))
 
-            cv2.imshow('thrash', gray_thresh)
+        _, gray_thresh = cv2.threshold(circle_image, 160, 255, cv2.THRESH_BINARY_INV)
 
-            # gray_canny = cv2.Canny(gray_thresh, 0, 50)
+        # cv2.imshow('to to', circle_image)
 
-            (contours, _) = cv2.findContours(gray_thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # gray_canny = cv2.Canny(gray_thresh, 0, 50)
+        print('==========================================================================================')
 
-            for contour in contours:
-                (x, y, w, h) = cv2.boundingRect(contour)
-                if int(shape_x*0.4) < int(x+w/2) < int(shape_x*0.6) and int(shape_y*0.4) < int(y+h/2) < int(shape_y*0.6):
-                    if contour_is_circle([x, y, w, h]):
-                        break_loop = True
-                    cv2.rectangle(gray_thresh, (x,y), (x+w, y+h), color=0, thickness=cv2.FILLED)
-            if break_loop:
+        (contours, _) = cv2.findContours(gray_thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if not circle_bounding_rect(contours, erase_img_list=(gray_thresh), erase_color=0):
+            continue
+
+        (contours, _) = cv2.findContours(gray_thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # cv2.drawContours(circle_image, contours, -1, color=(21, 32, 255))
+        for contour in contours:
+            (x, y, w, h) = cv2.boundingRect(contour)
+            if w > digit_w_max or h > digit_h_max or h < digit_h_min or w < digit_w_min :
                 continue
-            (contours, _) = cv2.findContours(gray_thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(temp, contours, -1, color=(21, 32, 255))
+            if contour_is_circle([x, y, w, h]):
+                continue
+            cv2.rectangle(circle_image, (x, y), (x + w, y + h), (0, 255, 0), 1)
+            number = center(gray_thresh[y:y+h, x:x+w], (20, 20))
 
-            cv2.imshow('gray', gray_thresh)
-
-            for contour in contours:
-                (x, y, w, h) = cv2.boundingRect(contour)
-                print(x, y, w, h)
-                if w > digit_w_max or h > digit_h_max or h < digit_h_min or w < digit_w_min :
-                    print('no ladnie')
-                    continue
-                if contour_is_circle([x, y, w, h]):
-                    continue
-                print('to spoko')
-                cv2.rectangle(temp, (x, y), (x + w, y + h), (0, 255, 0), 1)
-
-            cv2.imshow('okienko :>', temp)
+            cv2.imshow('okienko :>', number)
             cv2.waitKey()
+        # cv2.imshow('haha', circle_image)
+        # cv2.waitKey()
+        # cv2.imwrite(f'out\\{number}.jpg', temp)
 
-            # cv2.imwrite(f'out\\{number}.jpg', temp)
-            # number += 1
-        except:
-            print('tu mi nie wyszlo')
-            pass
-    cv2.imshow('okienko :>', image)
-    cv2.waitKey()
+    pass
 
+
+def resize(image, width=None, height=None, interpolation=cv2.INTER_AREA):
+    h, w = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        ratio = height / float(h)
+        dim = (int(w * ratio), height)
+    else:
+        ratio = width / float(w)
+        dim = (width, int(h * ratio))
+
+    return cv2.resize(image, dim, interpolation=interpolation)
+
+
+def center(image, des_shape):
+    des_image = np.zeros(des_shape, dtype="uint8")
+
+    h, w = image.shape[:2]
+    if h > w:
+        image = resize(image, height=des_shape[0])
+    else:
+        image = resize(image, width=des_shape[1])
+
+    offset_x = (des_shape[1] - image.shape[1]) // 2
+    offset_y = (des_shape[0] - image.shape[0]) // 2
+
+    des_image[offset_y:offset_y + image.shape[0], offset_x:offset_x + image.shape[1]] = image
+    return des_image
 
 
 if __name__ == '__main__':
